2955489f29f70b84831c1ab94e1da8b4d327dc6e
dinocoww <nabiilah.afiifah26@gmail.com>
2023-01-18 16:18:05 +0700

    ulang

--- php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start(); //inisialisasi session
-
-$_SESSION = [];
-
-if (session_destroy()) { //menghapus session
-    header("Location: ../index.php"); //jika berhasil maka akan di redirect ke file index.php
-}
